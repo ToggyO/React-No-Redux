417d3ce7c2c470a881e94ade3c9a97776fd2214a
Vitaly Ponomarev <ponomarev@magora-systems.com>
2019-10-09 19:38:16 +0700

    Temp commit
    Migration from macos

--- src/middleware.js
+++ src/middleware.js
@@ -1,43 +1,15 @@
 /* eslint-disable no-unused-vars */
-import Cookies from 'js-cookie';
 import { authTypes } from '@ducks/auth';
-import { meTypes } from '@ducks/me';
-import { globalTypes } from '@ducks/_global';
-import { userLogout } from '@services/auth';
 
-export const saveTokens = store => next => action => {
-  if (action.type === authTypes.TWO_FACTOR_AUTH_SUCCESS) {
-    const { accessToken, refreshToken } = action.payload.data;
-
-    Cookies.set('bdtoken', accessToken);
-    Cookies.set('bdrefreshtoken', refreshToken);
-  }
-  return next(action);
-};
+import { writeToLocalState } from '@services/ls';
 
 export const saveUserData = store => next => action => {
-  if (action.type === authTypes.SIGNIN_SUCCESS || action.type === authTypes.SIGNUP_SUCCESS) {
-    const { userName, id } = action.payload.data;
-
-    localStorage.setItem('userName', userName);
-    localStorage.setItem('userID', id);
+  if (action.type === authTypes.SIGNIN_SUCCESS) {
+    const { user, accessToken, refreshToken } = action.payload.data;
+    writeToLocalState('USER', user);
+    writeToLocalState('ACCESS_TOKEN', accessToken);
+    writeToLocalState('REFRESH_TOKEN', refreshToken);
   }
-  if (action.type === meTypes.GET_PROFILE_SUCCESS) {
-    const { userName, id } = action.payload.data.user;
-    const { countryCode } = action.payload.data;
-
-    localStorage.setItem('userName', userName);
-    localStorage.setItem('userID', id);
-    localStorage.setItem('countryCode', countryCode);
-  }
-  return next(action);
-};
-
-export const renewCountryDataOnTokenRefresh = store => next => action => {
-  if (action.type === globalTypes.REFRESHING_TOKEN_SUCCESS) {
-    store.dispatch({ type: meTypes.GET_PROFILE_REQUEST });
-  }
-
   return next(action);
 };
 
