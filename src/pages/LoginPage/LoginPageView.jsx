66303bc8095cc4adff6941c5db5af316cdab6f9f
Oleg <togushakov@magora-systems.com>
2019-10-11 17:37:58 +0700

    added form for sign up (step one)"

--- src/pages/LoginPage/LoginPageView.jsx
+++ src/pages/LoginPage/LoginPageView.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
 
-import { FormTemplateView } from '../components/form/FormTemplate';
-import { TextInput } from '../components/form/TextInput';
-import { validateField } from '../components/form/validation';
+import { SignUpStepOne } from '../components/form/SignUpForms/SignUpStepOne';
 
 import style from './style.module.sass';
 
@@ -26,46 +23,7 @@ const LoginPageView = () => (
       </div>
       <div className={`${style.form} flex justify-content-center align-items-center`}>
 
-        <FormTemplateView
-          titleLarge="Create your account"
-          titleSmall="Sign up with your work email or your Google Account."
-          link="Already have a Squad account? Log in here."
-        >
-          <Formik
-            // initialValues={user /** { email, social } */}
-            onSubmit={() => {}}
-            render={({ errors, status, touched, isSubmitting }) => (
-              <Form>
-                <Field
-                  type="email"
-                  name="google_mail"
-                  placeholder="Continue with Google"
-                  imgbefore="../../assets/login_page/email_icon.png"
-                  component={TextInput}
-                  validate={validateField.email}
-                />
-                <ErrorMessage name="google_mail" component="div" className="formik-error error-label"/>
-                <span style={{ color: '#9398A2', fontSize: 15, lineHeight: '21px', fontWeight: 400 }}>Or</span>
-                <Field
-                  type="email"
-                  name="user_mail"
-                  placeholder="name@company.com"
-                  imgbefore="../../assets/login_page/email_icon.png"
-                  component={TextInput}
-                  validate={validateField.email}
-                />
-                <ErrorMessage name="user_mail" component="div" className="formik-error error-label"/>
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="btn green rounded p-4 full_width"
-                >
-                  Next
-                </button>
-              </Form>
-            )}
-          />
-        </FormTemplateView>
+        <SignUpStepOne />
 
       </div>
     </div>
---
+++ src/pages/components/form/SignUpForms/SignUpStepOne/index.js
@@ -0,0 +1,1 @@
+export { default as SignUpStepOne } from './SignUpStepOne';
