f5926d296230282f824d87386e7613e662befb79
Oleg <togushakov@magora-systems.com>
2019-10-17 17:02:29 +0700

    [*]#SQUAD-44 - Add responsiveness for account page

--- src/pages/SignPages/ConfirmEmailPage/_components/ConfirmEmailForm/ConfirmEmailFormView.jsx
+++ src/pages/SignPages/ConfirmEmailPage/_components/ConfirmEmailForm/ConfirmEmailFormView.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 import { FormTemplateView } from '@components/Form/FormTemplate';
 import { ConfirmEmailInput } from '@components/Form/ConfirmEmailInput';
 
-const ConfirmEmailFormView = () => {
-  return (
-    <FormTemplateView
-      titleLarge="Confirm your email"
-      titleSmall="We have sent a confirmation code to name@company.com. Enter this code below:"
-      link="Didn’t receive a confirmation code? Click here"
-    >
-      <ConfirmEmailInput name="confirmEmail" maxLength={6} addClassWrapper="pt-4 pb-4" />
-    </FormTemplateView>
-  );
-};
+const ConfirmEmailFormView = () => (
+  <FormTemplateView
+    titleLarge="Confirm your email"
+    titleSmall="We have sent a confirmation code to name@company.com. Enter this code below:"
+    link="Didn’t receive a confirmation code? Click here"
+  >
+    <ConfirmEmailInput name="confirmEmail" maxLength={6} addClassWrapper="pt-4 pb-4"/>
+  </FormTemplateView>
+);
 
 export default ConfirmEmailFormView;
