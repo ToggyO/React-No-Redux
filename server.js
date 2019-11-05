a3e16d352cd2392354694244a994d94cb79b6441
Oleg <togushakov@magora-systems.com>
2019-11-05 15:39:48 +0700

    [*] -  Refactoring

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const dotenv = require('dotenv');
+const express = require('express');
+
+const app = express();
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root(`.env.${process.env.NODE_ENV}`) });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen({host: process.env.HOST, port: process.env.PORT}, () => {
+  console.log(`Server is running on:
+     host: ${process.env.HOST}
+     port: ${process.env.PORT}`);
+});
