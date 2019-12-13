a44e99effc9e91f43b283c5fab4162f877c311ca
Oleg <togushakov@magora-systems.com>
2019-12-13 20:12:11 +0700

    [+]#SQUAD-250 - Added change user name validation

--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -104,6 +104,10 @@ module.exports = merge(common, {
           },
         ],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
 });
